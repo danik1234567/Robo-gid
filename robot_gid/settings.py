c7152cabbef88c36220afb666bed99051599c262
Sam <51040158+SememN@users.noreply.github.com>
2021-02-13 15:53:27 +0500

    adding kvants app into INSTALLED_SETTINGS

--- robot_gid/settings.py
+++ robot_gid/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
+    'kvants.apps.KvantsConfig',
 ]
 
 MIDDLEWARE = [
