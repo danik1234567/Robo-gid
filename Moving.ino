61d89e3a7fc0caa22df8e08f2376275d030b375e
tovarishchvamtutdelatnechego <67748562+tovarishchvamtutdelatnechego@users.noreply.github.com>
2021-04-19 19:15:41 +0500

    Update Moving.ino

--- Moving.ino
+++ Moving.ino
@@ -5,45 +5,59 @@ GStepper< STEPPER2WIRE> stepperR(1024, 5, 6, 7);
 
 int command;
 int distanse = 1024;
+int listanse;
+int ristanse;
 
 void doing(int comand)
 {
 
+
+
   if (comand == -5) {
     distanse = distanse + 1024;
+    Serial.println("Дистанция увеличена, милорд");
   }
-  else if (comand == -3) {
+  else if (comand == -3 && distanse > 1024) {
     distanse = distanse - 1024;
-    if (distanse < 0)
-      distanse = -distanse;
+    Serial.println("Дистанция уменьшена, милорд");
   }
 
+  else if (comand == 5) {
+    if (Serial.available()) {
+      distanse = Serial.readString().toInt();
+      Serial.println("New distanse!");
+    }
+    Serial.println("Change distanse!");
+  }
   else if (comand == 8) {
-    stepperL.setTarget(-distanse, RELATIVE);
-    stepperR.setTarget(distanse, RELATIVE);
+    listanse = -distanse;
+    ristanse = distanse;
   }
   else if (comand == 2) {
-    stepperL.setTarget(distanse, RELATIVE);
-    stepperR.setTarget(-distanse, RELATIVE);
-  }
-  else if (comand == 6) {
-    stepperL.setTarget(-distanse, RELATIVE);
-    stepperR.setTarget(-distanse, RELATIVE);
+    listanse = distanse;
+    ristanse = -distanse;
   }
   else if (comand == 4) {
-    stepperL.setTarget(distanse, RELATIVE);
-    stepperR.setTarget(distanse, RELATIVE);
+    listanse = distanse;
+    ristanse = distanse;
+  }
+  else if (comand == 6) {
+    listanse = -distanse;
+    ristanse = -distanse;
   }
   else if (comand == 0) {
-    stepperL.setTarget(0, RELATIVE);
-    stepperR.setTarget(0, RELATIVE);
+    listanse = 0;//listanse / distanse;
+    ristanse = 0;//ristanse / distanse;
   }
+
+  stepperL.setTarget(listanse, RELATIVE);
+  stepperR.setTarget(ristanse, RELATIVE);
 }
 
 void setup() {
   Serial.begin(115200);
   stepperL.setRunMode(FOLLOW_POS);
-  stepperL.setMaxSpeed(400);
+  stepperL.setMaxSpeed(1000);
   stepperL.setAcceleration(500);
 
   stepperR.setRunMode(FOLLOW_POS);
@@ -52,13 +66,15 @@ void setup() {
 }
 void loop() {
   if (Serial.available()) {
-    command = Serial.read() - 48;
+    command = Serial.read() - 48; //String().toInt();
     Serial.println(command);
   }
 
-  if (command == 0)
+  if (command == 0) {
     doing(command);
-  else if (!stepperL.tick() and !stepperR.tick()) {
+  }
+  else if (!stepperL.tick() || !stepperR.tick()) {
     doing(command);
+    command = 0;
   }
 }
